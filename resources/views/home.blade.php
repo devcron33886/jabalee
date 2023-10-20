b409a2eb23214573a76472bb82d3634c7731a93c
LofiCoders <71966425+devcron33886@users.noreply.github.com>
2023-10-20 20:19:19 +0200

    added footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -116,12 +116,11 @@
             <h2 class="text-center text-lg font-semibold leading-8 text-white">Our most valued Partner</h2>
             <div
                 class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                    src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg" alt="Transistor" width="158"
-                    height="48">
-                <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                    src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg" alt="Reform" width="158"
-                    height="48">
+                <img class="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
+                    src="{{ asset('images/rhino.png') }}" alt="Rhino" width="158" height="48">
+
+                <img class="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
+                    src="{{ asset('images/smark.png') }}" alt="Smark" width="158" height="48">
                 <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
                     src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg" alt="Tuple" width="158"
                     height="48">
